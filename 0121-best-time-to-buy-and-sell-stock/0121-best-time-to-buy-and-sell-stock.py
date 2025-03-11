3b14a0700195270ee041b0868dac4f5880d7d027
cao-exe <146871579+cao-exe@users.noreply.github.com>
2025-03-11 17:44:13 -0400

    Time: 59 ms (66.39%), Space: 19.4 MB (36.61%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_profit = 0
+        lowest = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < lowest:
+                lowest = prices[i]
+            elif prices[i] - lowest > max_profit:
+                max_profit = prices[i] - lowest
+        return max_profit
+                
